67acb58fbf6a62f133235b5939c9f6f2d837f8a7
JuniYadi <juniyadi94@gmail.com>
2020-06-02 01:40:50 +0700

    Update Default Model

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,14 +1,25 @@
-import mongoose from 'mongoose';
-import Database from '../config/database'
-
-// Make Connection to Database
-new Database()
+import mongoose, { Schema, Document } from 'mongoose';
 
 // Table Name
 const table = 'table-name'
 
+// Interfaces Schema
+export interface IModel extends Document {
+    title: string,
+    body: string,
+    user_id: string,
+    categories: string,
+    tags: string
+}
+
 // Table Schema Collection
-const schema = new mongoose.Schema({})
+const schema = new mongoose.Schema(
+{
+    //
+},
+{
+    timestamps: true
+})
 
 // Export Model
-export default mongoose.model(table, schema)
+export default mongoose.model<IModel>(table, schema)
