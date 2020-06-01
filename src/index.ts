27308ff4b7bab9784b34cb3773ec686655b464ea
JuniYadi <juniyadi94@gmail.com>
2020-06-01 22:51:00 +0700

    Update Index Using Morgan

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express'
+import morgan from 'morgan'
 import web from './routes/web'
+import * as dotenv from 'dotenv';
+
+// Setup Envroitment From .env
+dotenv.config()
 
 // Setup Instance Express
 const app = express()
 
 // Middleware Body Parser
 app.use(express.json())
+
+// Debug With Morgan If Not In Production
+if(process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
 
 // Setup Global Routes
 app.use(web)
