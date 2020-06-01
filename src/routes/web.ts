95c9bc7dfd60c962c5cd0dd472863c648a7b4937
JuniYadi <juniyadi94@gmail.com>
2020-06-02 01:39:25 +0700

    Update Web Routes Error Handling

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,8 +1,25 @@
-import express from 'express'
-import blog from './v1/blog'
+import express, {Request, Response, NextFunction} from 'express'
+import Blog from './v1/Blog'
+import Auth from './v1/Auth'
 
-const web = express()
+const web = express.Router()
 
-web.use('/blog', blog)
+// routes
+web.use('/blog', Blog)
+web.use('/auth', Auth)
+
+// Error Handling
+interface Error {
+    status?: number;
+    message?: string;
+}
+
+web.use(( error: Error , req: Request, res: Response, next: NextFunction) => {
+	res.status(422).json({
+        code: 422,
+        success: false,
+        message: error.message
+    });
+});
 
 export default web
