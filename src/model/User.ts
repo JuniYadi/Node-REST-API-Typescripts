d70e4acd814332962ad4a8e93083a7450a45e385
JuniYadi <juniyadi94@gmail.com>
2020-06-02 11:01:43 +0700

    Update User Model

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
 
 // Table Name
 const table = 'user'
 
-// Allow Index
-mongoose.set('useCreateIndex', true)
+// Table Types
+export type UserDocument = mongoose.Document & {
+    name: string;
+    email: string;
+    password: string;
+    passwordResetToken: string;
+    passwordResetExpires: Date;
+
+    comparePassword: comparePasswordFunction;
+}
 
 // Table Schema Collection
 const schema = new mongoose.Schema({
@@ -21,10 +30,26 @@ const schema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    passwordResetToken: String,
+    passwordResetExpires: Date
 },
 {
     timestamps: true
 })
+
+// Compare Password Type
+type comparePasswordFunction = (candidatePassword: string, cb: (err: any, isMatch: any) => {}) => void;
+
+// Compare Password Function
+const comparePassword: comparePasswordFunction = function (this: any, candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, (err: mongoose.Error, isMatch: boolean) => {
+        cb(err, isMatch);
+    });
+};
+
+// Inject Compare password to Mongoose Schema
+schema.methods.comparePassword = comparePassword;
+
 // Export Model
-export default mongoose.model(table, schema)
+export const User = mongoose.model<UserDocument>(table, schema)
