472b35b62609d962d1448cd9a2f4e95e814a4b7c
JuniYadi <juniyadi94@gmail.com>
2020-06-04 04:16:06 +0700

    Update Auth Routes Using Middleware

--- src/routes/v1/Auth.ts
+++ src/routes/v1/Auth.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-import { register, login } from '../../controllers/Auth'
+import { register, login, me } from '../../controllers/Auth'
+import AuthMiddleware from '../middleware/Auth'
 
 // make express routes
 const auth = express.Router()
 
 auth.post('/register', register)
 auth.post('/login', login)
+auth.get('/me', AuthMiddleware, me)
 
 export default auth
