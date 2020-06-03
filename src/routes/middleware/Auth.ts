441bc39fc9bec24896f35217a400317b4aacc57a
JuniYadi <juniyadi94@gmail.com>
2020-06-04 04:15:39 +0700

    Added Auth Route Middleware

---
+++ src/routes/middleware/Auth.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Middleware Default Error Handling
+export default (req: Request, res: Response, next: NextFunction) => {
+    // token data
+    let token: any;
+    // get secret keys
+    const jsonSecret = process.env.JSON_SECRET || 'secret-keys-default'
+
+    // check header token or get from query token
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+        token = req.query.token;
+    }
+
+    // if token not null, then vaidate token
+    if (token) {
+        jwt.verify(token, jsonSecret, (e: any, data: any) => {
+            // check if error result
+            if (e) {
+                return next(e)
+            }
+
+            // inject userID to Request
+            req.body["id"] = data.id
+
+            // continue
+            next()
+        })
+    } else {
+        return res.status(401).send({
+            "code": 401,
+            "success": false,
+            "message": "Unauthorized."
+        });
+    }
+}
